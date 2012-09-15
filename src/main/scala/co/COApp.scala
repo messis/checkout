0f5f25b338ced87706c17a60560402154a334adf
Karsten Mevassvik <karsten.mevassvik@finn.no>
2012-09-16 01:21:53 +0200

    Changed port

--- src/main/scala/co/COApp.scala
+++ src/main/scala/co/COApp.scala
@@ -1,5 +1,9 @@
 package co
 
+import util.Properties
+
 object COApp extends App {
-  unfiltered.jetty.Http(19901).plan(new COPlan).run()
+  val port = Properties.envOrElse("PORT", "5000").toInt
+
+  unfiltered.jetty.Http(port).plan(new COPlan).run()
 }
