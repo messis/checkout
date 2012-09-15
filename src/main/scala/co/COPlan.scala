77a0d7ecbcdb2d3bd664c947098e3ed9af0921f8
Karsten Mevassvik <karsten.mevassvik@finn.no>
2012-09-15 23:40:08 +0200

    more strapping

--- src/main/scala/co/COPlan.scala
+++ src/main/scala/co/COPlan.scala
@@ -1,12 +1,11 @@
 package co
 
-import _root_.templates.FrontpageTemplate
 import unfiltered.filter.Plan
 import unfiltered.request.GET
 import unfiltered.response.{Html5, HtmlContent, ResponseString}
 
 class COPlan extends Plan {
   def intent = {
-    case GET(_) => Html5(FrontpageTemplate.bootstrap)
+    case GET(_) => Html5(Pages.index)
   }
 }
