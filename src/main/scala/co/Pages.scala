49884fa77623e666f600db00ce2db79df89c7d20
messsis <karsten@mevassvik.com>
2012-09-17 00:20:00 +0200

    GA added

--- src/main/scala/co/Pages.scala
+++ src/main/scala/co/Pages.scala
@@ -92,6 +92,20 @@ object Pages {
         <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-114-precomposed.png"/>
         <link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-72-precomposed.png"/>
         <link rel="apple-touch-icon-precomposed" href="http://twitter.github.com/bootstrap/assets/ico/apple-touch-icon-57-precomposed.png"/>
+        <script type="text/javascript">
+          {"""
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-34868343-1']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+            var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+            ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+            var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+          """}
+        </script>
+
       </head>
 
       <body>
